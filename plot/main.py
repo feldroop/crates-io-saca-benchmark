05aca99e7198b4e04fca362463538b9976e05b36
Felix Leander Droop <info@felix-droop.de>
2025-08-27 18:47:43 +0200

    slightly change plot

--- plot/main.py
+++ plot/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def main():
-    names = ["libsais (i32)", "libsais 8 threads (i32)", "libsais (i64)",  "libsais 8 threads (i64)", "divsufsort (i32)", "suffix (u32)", "bio (usize)", "psacak (u32)", "psacak 8 threads (u32)", "sais-drum (usize)"]
+    names = ["libsais (i32)", "libsais 8 threads (i32)", "libsais (i64)",  "libsais 8 threads (i64)", "divsufsort (i32)", "suffix (str input, u32)", "bio (usize)", "psacak (u32)", "psacak 8 threads (u32)", "sais-drum (usize)"]
     x = list(range(10))
     running_times = [78, 26, 86, 29, 185, 364, 1092, 281, 131, 353]
     peak_memory_usages = [8, 8, 16, 16, 8, 11.2, 21.6, 8, 8, 16.2]
@@ -10,7 +10,7 @@ def main():
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
     
     bars1 = ax1.bar(x, running_times, color=colors)
-    ax1.set_title("Running times in seconds")
+    ax1.set_title("Running time in seconds")
     ax1.set_xticks([]) 
     ax1.bar_label(bars1)
     ax1.set_ylabel("seconds")
